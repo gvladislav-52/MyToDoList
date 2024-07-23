4f12b8cf5db572b0c66586b942d5081035efb3a7
gvladislav-52 <gvladislav_52@mail.ru>
2024-07-23 20:34:26 +0300

    first commit

---
+++ ToDoListPractics/Extensions.swift
@@ -0,0 +1,8 @@
+//
+//  Extensions.swift
+//  ToDoListPractics
+//
+//  Created by gvladislav-52 on 23.07.2024.
+//
+
+import Foundation
---
+++ ToDoListPractics/ToDoCell.swift
@@ -0,0 +1,8 @@
+//
+//  ToDoCell.swift
+//  ToDoListPractics
+//
+//  Created by gvladislav-52 on 23.07.2024.
+//
+
+import Foundation
