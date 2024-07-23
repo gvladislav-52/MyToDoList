543f76eda7f7d6a1a0c9ef989ae7f5a8af8ab81e
gvladislav-52 <gvladislav_52@mail.ru>
2024-07-23 14:14:59 +0300

    Initial Commit

---
+++ ToDoListPractics/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoListPractics
+//
+//  Created by gvladislav-52 on 23.07.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
